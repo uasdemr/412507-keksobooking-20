5ce4278e880896069fd36283604650e4ece410aa
uasdemr <sdyuzhilin@mail.ru>
2020-06-05 19:49:53 +0300

    пофиксил стиллистические баги 2

--- js/main.js
+++ js/main.js
@@ -1,62 +1,55 @@
 'use strict';
 
 function objGererator() {
-  //Массив объектов, который вернем заполненным
   var arr = [];
 
-  //Генерит случайное число в диапазоне min, max
   var getRandomArbitrary = function (min, max) {
-    return parseInt(Math.random() * (max - min) + min);
+    return parseInt(Math.random() * (max - min) + min, 10);
   };
 
   var types = ['palace', 'flat', 'house', 'bungalo'];
-  var checkins = ["12:00", "13:00", "14:00"];
-  var checkout = ["12:00", "13:00", "14:00"];
+  var checkins = ['12:00', '13:00', '14:00'];
+  var checkout = ['12:00', '13:00', '14:00'];
   var photos = [
-        "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-        "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-        "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-      ];
-  var features = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-
-  //Длина карты
+    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+  ];
+  var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
   var map = document.querySelector('.map');
   var mapWidth = document.querySelector('.map').clientWidth;
   map.classList.remove('map--faded');
 
-  //Заполняем массив
-  for(var i = 1; i < 9; i++) {
+  for (var i = 1; i < 9; i++) {
     arr.push(
-      {
-        "author": {
-          "avatar": "img/avatars/user0" + i + ".png",
-        },
-        "offer": {
-          "title": "строка, заголовок предложения",
-          "address": "" + getRandomArbitrary(0, 600) + ", " + getRandomArbitrary(0, 600),
-          "price": getRandomArbitrary(300, 2500),
-          "type": types[parseInt(getRandomArbitrary(0, types.length))],
-          "rooms": getRandomArbitrary(1, 5),
-          "guests": getRandomArbitrary(1, 10),
-          "checkin": checkins[parseInt(getRandomArbitrary(0, checkins.length))],
-          "checkout": checkout[parseInt(getRandomArbitrary(0, checkout.length))],
-          "features": features[parseInt(getRandomArbitrary(0, features.length))],
-          "description": "строка с описанием",
-          "photos": photos[parseInt(getRandomArbitrary(0, photos.length))],
-        },
-        "location": {
-          "x": getRandomArbitrary(0, parseInt(mapWidth)),
-          "y": getRandomArbitrary(130, 630)
+        {
+          'author': {
+            'avatar': 'img/avatars/user0' + i + '.png',
+          },
+          'offer': {
+            'title': 'строка, заголовок предложения',
+            'address': '' + getRandomArbitrary(0, 600) + ', ' + getRandomArbitrary(0, 600),
+            'price': getRandomArbitrary(300, 2500),
+            'type': types[getRandomArbitrary(0, types.length)],
+            'rooms': getRandomArbitrary(1, 5),
+            'guests': getRandomArbitrary(1, 10),
+            'checkin': checkins[getRandomArbitrary(0, checkins.length)],
+            'checkout': checkout[getRandomArbitrary(0, checkout.length)],
+            'features': features[getRandomArbitrary(0, features.length)],
+            'description': 'строка с описанием',
+            'photos': photos[getRandomArbitrary(0, photos.length)],
+          },
+          'location': {
+            'x': getRandomArbitrary(0, mapWidth),
+            'y': getRandomArbitrary(130, 630)
+          }
         }
-      }
     );
 
   }
-  //Возвращаем заполненный массив
   return arr;
 }
 var data = objGererator();
-//console.log(data);
 
 var template = document.querySelector('#pin').content;
 var templateButton = template.querySelector('.map__pin');
