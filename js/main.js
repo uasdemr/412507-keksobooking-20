dbb27b403fd4e023bce3369ed481c1667e457955
uasdemr <sdyuzhilin@mail.ru>
2020-06-07 23:55:31 +0300

    Личный проект: больше деталей (часть 2), все функции написаны, картинка отображается, лишний img тэг удаляется

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 'use strict';
 
+// Функция генерации 8-ми объектов упакованных в массив
 function objGererator() {
   var arr = [];
 
+  // Функция генерирует случайные числа в заданном диапазоне
   var getRandomArbitrary = function (min, max) {
     return parseInt(Math.random() * (max - min) + min, 10);
   };
@@ -35,9 +37,9 @@ function objGererator() {
             'guests': getRandomArbitrary(1, 10),
             'checkin': checkins[getRandomArbitrary(0, checkins.length)],
             'checkout': checkout[getRandomArbitrary(0, checkout.length)],
-            'features': features[getRandomArbitrary(0, features.length)],
+            'features': features,
             'description': 'строка с описанием',
-            'photos': photos[getRandomArbitrary(0, photos.length)],
+            'photos': photos
           },
           'location': {
             'x': getRandomArbitrary(0, mapWidth),
@@ -51,11 +53,12 @@ function objGererator() {
 }
 var data = objGererator();
 
-var template = document.querySelector('#pin').content;
-var templateButton = template.querySelector('.map__pin');
+var pinTemplate = document.querySelector('#pin').content;
+var templateButton = pinTemplate.querySelector('.map__pin');
 var mapPins = document.querySelector('.map__pins');
 
-var domElementMaker = function (item) {
+//Создаем пин
+var domPinElementMaker = function (item) {
   var mapPin = templateButton.cloneNode(true);
   var mapPinImg = mapPin.querySelector('img');
   mapPinImg.src = item.author.avatar;
@@ -66,9 +69,121 @@ var domElementMaker = function (item) {
   return mapPin;
 };
 
+// Заполняем DOM пинами
 function domRender() {
   for (var i = 0; i < data.length; i++) {
-    mapPins.appendChild(domElementMaker(data[i]));
+    mapPins.appendChild(domPinElementMaker(data[i]));
   }
 }
 domRender();
+
+var adTemplate = document.querySelector('#card').content;
+var mapsArticle = adTemplate.querySelector('.map__card').cloneNode(true);
+
+// Супер-функция создания карточки объявления, наверняка можно разнести на 2 а то и три разные
+var domCardElementMaker = function (data) {
+  // Инициализация переменных для карточки описания
+  var card = mapsArticle.cloneNode(false);
+  var cardTitle = mapsArticle.querySelector('.popup__title').cloneNode(true);
+  var cardAddress = mapsArticle.querySelector('.popup__text--address').cloneNode(true);
+  var cardPrice = mapsArticle.querySelector('.popup__text--price').cloneNode(true);
+  var cardPopupType = mapsArticle.querySelector('.popup__type').cloneNode(true);
+  var cardCapacity = mapsArticle.querySelector('.popup__text--capacity').cloneNode(true);
+  var cardTextTime = mapsArticle.querySelector('.popup__text--time').cloneNode(true);
+  var cardFeatures = mapsArticle.querySelector('.popup__features').cloneNode(true);
+  var cardDescription = mapsArticle.querySelector('.popup__description').cloneNode(true);
+  var cardPhotos = mapsArticle.querySelector('.popup__photos').cloneNode(true);
+  var cardAvatar = mapsArticle.querySelector('.popup__avatar').cloneNode(true);
+
+  // Заполнение переменных данными из массива объектов
+  cardTitle.textContent = data.offer.title;
+  cardAddress.textContent = data.offer.address;
+  cardPrice.textContent = data.offer.price + ' ₽/ночь';
+
+  // Выводим красиво вид жилища
+  var apartments = '';
+  switch (data.offer.type) {
+    case 'palace': apartments = 'Дворец';
+      break;
+    case 'flat': apartments = 'Квартира';
+      break;
+    case 'house': apartments = 'Дом';
+      break;
+    case 'bungalo': apartments = 'Бунгало';
+      break;
+
+    default: apartments = 'Палатка';
+      break;
+  }
+  cardPopupType.textContent = apartments + ' ₽/ночь';
+  cardCapacity.textContent = data.offer.rooms + ' комнаты для ' + data.offer.guests + ' гостей';
+  cardTextTime.textContent = 'Заезд после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
+
+  var feature = '';
+  var myString = '';
+  for (var n = 0; n < data.offer.features.length; n++) {
+    // Выводим красиво список улучшений жилища
+    switch (data.offer.features[n]) {
+      case 'wifi': feature = 'Wi-Fi';
+        break;
+      case 'dishwasher': feature = 'Посудомоечная машина';
+        break;
+      case 'parking': feature = 'Парковка';
+        break;
+      case 'washer': feature = 'Стиральная машина';
+        break;
+      case 'elevator': feature = 'Лифт';
+        break;
+      case 'conditioner': feature = 'Кондиционер';
+        break;
+
+      default: feature = 'Ничего нет, вы изжаритесь тут от жары и похудеете от голода';
+        break;
+    }
+    myString += feature + ' ';
+  }
+  cardFeatures.textContent = myString;
+  cardDescription.textContent = data.offer.description;
+  cardAvatar.src = data.author.avatar;
+
+  // Заполняем cardPhotos коллекцией заполненных img текущего объекта
+  for (var j = 0; j < data.offer.photos.length; j++) {
+    var cardImg = mapsArticle.querySelector('.popup__photos').querySelector('.popup__photo').cloneNode(true);
+    cardImg.src = data.offer.photos[j];
+    cardPhotos.appendChild(cardImg);
+  }
+
+  // Добавляем к карточке объявления- заполненные переменные
+  card.appendChild(cardTitle);
+  card.appendChild(cardAddress);
+  card.appendChild(cardPrice);
+  card.appendChild(cardPopupType);
+  card.appendChild(cardCapacity);
+  card.appendChild(cardTextTime);
+  card.appendChild(cardFeatures);
+  card.appendChild(cardDescription);
+
+  var imgs = cardPhotos.getElementsByTagName('img');
+  // Этот прекрасный цикл удаляет все img с пустым src, которые любезно подсовывает академия
+  for (var m = 0; m < imgs.length; m++) {
+    if (imgs[m].src.includes('localhost')) {
+      imgs[m].remove();
+    }
+  }
+  card.appendChild(cardPhotos);
+  card.appendChild(cardAvatar);
+
+  // Возвращаем заполненную карточку
+  return card;
+};
+
+var mainMap = document.querySelector('.map');
+var mapFiltersContainer = document.querySelector('.map__filters-container');
+
+// Добавляем карточки в цикле в дом
+function domCardRender(data) {
+  for (var k = 0; k < data.length; k++) {
+    mainMap.insertBefore(domCardElementMaker(data[k]), mapFiltersContainer);
+  }
+}
+domCardRender(data);
