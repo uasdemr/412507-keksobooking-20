202647061563d04b769de92783c70c720b25b35b
uasdemr <sdyuzhilin@mail.ru>
2020-06-08 16:52:50 +0300

    Критерии Б5, Б8

--- js/main.js
+++ js/main.js
@@ -2,28 +2,28 @@
 
 // Функция генерации 8-ми объектов упакованных в массив
 function objGererator() {
-  var arr = [];
+  var Arr = [];
 
   // Функция генерирует случайные числа в заданном диапазоне
   var getRandomArbitrary = function (min, max) {
     return parseInt(Math.random() * (max - min) + min, 10);
   };
 
-  var types = ['palace', 'flat', 'house', 'bungalo'];
-  var checkins = ['12:00', '13:00', '14:00'];
-  var checkout = ['12:00', '13:00', '14:00'];
-  var photos = [
+  var Types = ['palace', 'flat', 'house', 'bungalo'];
+  var Checkins = ['12:00', '13:00', '14:00'];
+  var Checkouts = ['12:00', '13:00', '14:00'];
+  var Photos = [
     'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
     'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
     'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
   ];
-  var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  var Features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
   var map = document.querySelector('.map');
   var mapWidth = document.querySelector('.map').clientWidth;
   map.classList.remove('map--faded');
 
   for (var i = 1; i < 9; i++) {
-    arr.push(
+    Arr.push(
         {
           'author': {
             'avatar': 'img/avatars/user0' + i + '.png',
@@ -32,14 +32,14 @@ function objGererator() {
             'title': 'строка, заголовок предложения',
             'address': '' + getRandomArbitrary(0, 600) + ', ' + getRandomArbitrary(0, 600),
             'price': getRandomArbitrary(300, 2500),
-            'type': types[getRandomArbitrary(0, types.length)],
+            'type': Types[getRandomArbitrary(0, Types.length)],
             'rooms': getRandomArbitrary(1, 5),
             'guests': getRandomArbitrary(1, 10),
-            'checkin': checkins[getRandomArbitrary(0, checkins.length)],
-            'checkout': checkout[getRandomArbitrary(0, checkout.length)],
-            'features': features,
+            'checkin': Checkins[getRandomArbitrary(0, Checkins.length)],
+            'checkout': Checkouts[getRandomArbitrary(0, Checkouts.length)],
+            'Features': Features,
             'description': 'строка с описанием',
-            'photos': photos
+            'Photos': Photos
           },
           'location': {
             'x': getRandomArbitrary(0, mapWidth),
@@ -49,9 +49,9 @@ function objGererator() {
     );
 
   }
-  return arr;
+  return Arr;
 }
-var data = objGererator();
+var Data = objGererator();
 
 var pinTemplate = document.querySelector('#pin').content;
 var templateButton = pinTemplate.querySelector('.map__pin');
@@ -69,11 +69,13 @@ var domPinElementMaker = function (item) {
   return mapPin;
 };
 
-// Заполняем DOM пинами
+// Заполняем DOM пинами через documentFragment
 var domRender = function () {
-  for (var i = 0; i < data.length; i++) {
-    mapPins.appendChild(domPinElementMaker(data[i]));
+  var pinsFragment = document.createDocumentFragment();
+  for (var i = 0; i < Data.length; i++) {
+    pinsFragment.appendChild(domPinElementMaker(Data[i]));
   }
+  mapPins.appendChild(pinsFragment);
 };
 domRender();
 
@@ -81,7 +83,7 @@ var adTemplate = document.querySelector('#card').content;
 var mapsArticle = adTemplate.querySelector('.map__card').cloneNode(true);
 
 // Супер-функция создания карточки объявления, наверняка можно разнести на 2 а то и три разные
-var domCardElementMaker = function (datas) {
+var domCardElementMaker = function (dataSet) {
   // Инициализация переменных для карточки описания
   var card = mapsArticle.cloneNode(false);
   var cardTitle = mapsArticle.querySelector('.popup__title').cloneNode(true);
@@ -96,13 +98,13 @@ var domCardElementMaker = function (datas) {
   var cardAvatar = mapsArticle.querySelector('.popup__avatar').cloneNode(true);
 
   // Заполнение переменных данными из массива объектов
-  cardTitle.textContent = datas.offer.title;
-  cardAddress.textContent = datas.offer.address;
-  cardPrice.textContent = datas.offer.price + ' ₽/ночь';
+  cardTitle.textContent = dataSet.offer.title;
+  cardAddress.textContent = dataSet.offer.address;
+  cardPrice.textContent = dataSet.offer.price + ' ₽/ночь';
 
   // Выводим красиво вид жилища
   var apartments = '';
-  switch (datas.offer.type) {
+  switch (dataSet.offer.type) {
     case 'palace': apartments = 'Дворец';
       break;
     case 'flat': apartments = 'Квартира';
@@ -116,14 +118,14 @@ var domCardElementMaker = function (datas) {
       break;
   }
   cardPopupType.textContent = apartments + ' ₽/ночь';
-  cardCapacity.textContent = datas.offer.rooms + ' комнаты для ' + datas.offer.guests + ' гостей';
-  cardTextTime.textContent = 'Заезд после ' + datas.offer.checkin + ', выезд до ' + datas.offer.checkout;
+  cardCapacity.textContent = dataSet.offer.rooms + ' комнаты для ' + dataSet.offer.guests + ' гостей';
+  cardTextTime.textContent = 'Заезд после ' + dataSet.offer.checkin + ', выезд до ' + dataSet.offer.checkout;
 
   var feature = '';
   var myString = '';
-  for (var n = 0; n < datas.offer.features.length; n++) {
+  for (var n = 0; n < dataSet.offer.Features.length; n++) {
     // Выводим красиво список улучшений жилища
-    switch (datas.offer.features[n]) {
+    switch (dataSet.offer.Features[n]) {
       case 'wifi': feature = 'Wi-Fi';
         break;
       case 'dishwasher': feature = 'Посудомоечная машина';
@@ -143,13 +145,13 @@ var domCardElementMaker = function (datas) {
     myString += feature + ' ';
   }
   cardFeatures.textContent = myString;
-  cardDescription.textContent = datas.offer.description;
-  cardAvatar.src = datas.author.avatar;
+  cardDescription.textContent = dataSet.offer.description;
+  cardAvatar.src = dataSet.author.avatar;
 
   // Заполняем cardPhotos коллекцией заполненных img текущего объекта
-  for (var j = 0; j < datas.offer.photos.length; j++) {
+  for (var j = 0; j < dataSet.offer.Photos.length; j++) {
     var cardImg = mapsArticle.querySelector('.popup__photos').querySelector('.popup__photo').cloneNode(true);
-    cardImg.src = datas.offer.photos[j];
+    cardImg.src = dataSet.offer.Photos[j];
     cardPhotos.appendChild(cardImg);
   }
 
@@ -181,9 +183,12 @@ var mainMap = document.querySelector('.map');
 var mapFiltersContainer = document.querySelector('.map__filters-container');
 
 // Добавляем карточки в цикле в дом
-var domCardRender = function domCardRender(datas) {
-  for (var k = 0; k < datas.length; k++) {
-    mainMap.insertBefore(domCardElementMaker(datas[k]), mapFiltersContainer);
+var domCardRender = function domCardRender(dataSet) {
+  var cardsFragment = document.createDocumentFragment();
+  for (var k = 0; k < dataSet.length; k++) {
+    // mainMap.insertBefore(domCardElementMaker(dataSet[k]), mapFiltersContainer);
+    cardsFragment.appendChild(domCardElementMaker(dataSet[k]));
   }
+  mainMap.insertBefore(cardsFragment, mapFiltersContainer);
 };
-domCardRender(data);
+domCardRender(Data);
