661561b781ab207281eee1e474805f15bd996cd8
uasdemr <sdyuzhilin@mail.ru>
2020-06-19 23:23:46 +0300

    Ограничение передвижения главного pin

--- js/mainpin.js
+++ js/mainpin.js
@@ -2,55 +2,69 @@
 
 window.mainPin = (function () {
   var mapPinMain = document.querySelector('.map__pin--main');
-  var startCoords = {};
-  var dragged = false;
+  var map = document.querySelector('.map');
+  var isDrag = false;
+
+  var limits = {
+    top: 130,
+    right: map.offsetWidth + map.offsetLeft - (mapPinMain.offsetWidth / 2),
+    bottom: 630,
+    left: map.offsetLeft + (mapPinMain.offsetWidth / 2)
+  };
+
+  var shiftY;
+  var shiftX;
 
   var mapPinMainMouseDownHandler = function (evt) {
     if (evt.button === 0) {
-      evt.preventDefault();
 
-      startCoords = {
-        x: evt.clientX,
-        y: evt.clientY
-      };
+      isDrag = true;
+      shiftY = evt.clientY - mapPinMain.getBoundingClientRect().top;
+      shiftX = evt.clientX - mapPinMain.getBoundingClientRect().left;
 
       mapPinMain.addEventListener('mousemove', mapPinMainMouseMoveHandler);
       mapPinMain.addEventListener('mouseup', mapPinMainMouseUpHandler);
     }
   };
 
-
   var mapPinMainMouseMoveHandler = function (evt) {
-    //evt.preventDefault();
+    if (isDrag) {
+      var newLocation = {
+        x: limits.left,
+        y: limits.top
+      };
 
-    dragged = true;
-
-    var shift = {
-      x: startCoords.x - evt.clientX,
-      y: startCoords.y - evt.clientY
-    };
-
-    startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
-
-    mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
-    mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+      if (evt.pageX > limits.right) {
+        newLocation.x = limits.right;
+      } else if (evt.pageX > limits.left) {
+        newLocation.x = evt.pageX;
+      }
+      if (evt.pageY > limits.bottom) {
+        newLocation.y = limits.bottom;
+      } else if (evt.pageY > limits.top) {
+        newLocation.y = evt.pageY;
+      }
+      relocate(newLocation);
+    }
   };
 
 
-  var mapPinMainMouseUpHandler = function (evt) {
+
+  var mapPinMainMouseUpHandler = function () {
+    isDrag = false;
     window.form.setAddress(mapPinMain);
     mapPinMain.removeEventListener('mousemove', mapPinMainMouseMoveHandler);
     mapPinMain.removeEventListener('mouseup', mapPinMainMouseUpHandler);
-
-    if (dragged) {
-
-    }
-
   };
 
+  //* размещение mapPinMain
+  function relocate(newLocation) {
+    mapPinMain.style.left = newLocation.x - shiftX + 'px';
+    mapPinMain.style.top = newLocation.y - shiftY + 'px';
+  }
+
   mapPinMain.addEventListener('mousedown', mapPinMainMouseDownHandler);
+
+
 
 })();
